da4f6bb26fcbb6556d01dcb133d2b8691fead6ca
Blake Brisben <blakebri@rams.colostate.edu>
2019-12-05 09:10:03 -0700

    Adding p12

---
+++ P12/src/R20.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class R20 {
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		LinkedBookList lbl = new LinkedBookList();
+		
+		Scanner scan = new Scanner(new File("top10.txt"));
+		int bookCount = (Integer.parseInt(scan.nextLine()));
+		
+		//reads in all of the books, and adds them to the linked list
+		for(int i = 0; i < bookCount; i++){
+			String line = scan.nextLine();
+			String[] parts = line.split("\t");
+			String title = parts[0];
+			String author = parts[1];
+			int numPages = Integer.parseInt(parts[2]);
+			Book b = new Book(title, author, numPages);
+			lbl.add(b);
+		}
+		
+		System.out.println("Testing add");
+		System.out.println(lbl);
+		
+		scan.close();
+	}
+}
